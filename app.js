814460ec81a9e71c28e721c44092e1db6e5c73ed
rodtob <rodrigo.toblli@gmail.com>
2020-09-07 13:46:28 -0300

    arreglo app js ruteo correcto

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static('static'))
+app.use(express.static(__dirname))
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + "/preindex.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
-app.get('index', (req,res)=>{
-    res.sendFile(__dirname + '/index.html')
+app.get('/home', (req,res)=>{
+    res.sendFile(__dirname + '/home.html')
 })
 
 app.get('*', (req,res)=>{
