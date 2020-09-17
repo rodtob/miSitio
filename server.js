b4e20e8e1392999145800e5fee4386d12be39912
rodtob <rodrigo.toblli@gmail.com>
2020-09-16 21:16:26 -0300

    servidor 5000

--- server.js
+++ server.js
@@ -16,7 +16,4 @@ app.get('*', (req,res)=>{
     res.sendFile(__dirname + '/error.html')
 })
 
-
-app.listen(3000,()=>{
-    console.log('Servidor funcionando')
-})
+app.listen(process.env.PORT || 5000)
